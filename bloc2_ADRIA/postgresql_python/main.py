4508cc5d43c790a56224af8b0791f878309ce05f
amelevila <amelevila78@gmail.com>
2025-02-05 10:13:46 +0100

    read registre i main

--- bloc2_ADRIA/postgresql_python/main.py
+++ bloc2_ADRIA/postgresql_python/main.py
@@ -1,4 +1,17 @@
 import create_registre as cr
+import read_registre as rr
 
 #Trucada per executar la funció a l'arxiu create_registre.py
 cr.create_reg()
+
+#Trucada per read_reg()
+results = rr.read_reg()
+print (results)
+
+#for i in results:
+ #   print('\n')
+  #  print('Nom: ' + i[1])
+   # print('Adreça: ' + i[2])
+    #print('telèfon: ' + i[3])
+    #print('email: ' + i[4])
+    #print('neixament: ' + i[5])
--- bloc2_ADRIA/postgresql_python/read_registre.py
+++ bloc2_ADRIA/postgresql_python/read_registre.py
@@ -4,11 +4,22 @@ def read_reg():
     conn = connect.connection_db()
     cursor = conn.cursor()
 
-    sql_read = "SELECT * FROM clientes2"
+    sql_read = "SELECT * FROM clientes"
 
     cursor.execute(sql_read)
     conn.commit()
 
-    results = cursor.fetchall
-
+    results = cursor.fetchall()
+    print(results)
+    print(results[4])
+    print(results[4][3])
+    print("\n")
+    print(results[9])
+    print(results[9][3])
+    print(results[14])
+    print(results[14][1])
+    print(results[19])
+    print(results[19][4])
     return results
+
+read_reg()
